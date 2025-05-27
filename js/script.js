169858c8803b8ba3b613e0418e5a42f3c44c15c2
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-05-27 17:30:34 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  let termOne = 2
+  let termTwo = 1
+  let termAnswer = 0
+  const numberOfTerms = parseInt(document.getElementById("number-of-terms").value)
+  if(numberOfTerms < 1) {
+    document.getElementById("answer").innerHTML = 
+      "Error, number must be positive integer"
+  }
+  else if (numberOfTerms = 1) {
+    document.getElementById("answer").innerHTML = 
+      "Number is: 2"
+  }
+  else if (numberOfTerms = 2) {
+    document.getElementById("answer").innerHTML = 
+      "Number is: 1"
+  }
+  else {
+    for (let counter = 2; counter < numberOfTerms; counter++) {
+      termAnswer = termOne + termTwo
+      termOne = termTwo
+      termTwo = termAnswer
+    }
+    document.getElementById("answer").innerHTML = 
+    "<p>Number is: " + termAnswer + ""
+  }
+}
